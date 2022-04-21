a8344ba388a251e265bcb035f7d67965a33f6598
MuhLibri <104043362+MuhLibri@users.noreply.github.com>
2022-04-21 14:52:11 +0700

    Update F17.py
    Menambah save pada exit

--- F17.py
+++ F17.py
@@ -1,12 +1,14 @@
 # F17 - Exit
 from functions import *
+from F16 import *
 
 def exit():
     print("Exit")
     print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
     s = input()
-
     while not cekExit(s):
         print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
         s = input()
+    if (s == "Y") or (s == "y"):
+        save   
     return
