b5f05988484657eb601399c953505c6bdf3022b3
Najmi Az-Zahra Feryputri <najmi.af24@gmail.com>
2022-04-23 23:08:05 +0700

    Update F04.py

--- F04.py
+++ F04.py
@@ -6,20 +6,20 @@ def tambahGame () :
     #Input data game yang ingin ditambahkan
     nama=input("Masukkan nama game: ")
     kategori=input("Masukkan kategori: ")
-    tahun_rilis=int(input("Masukkan tahun rilis: "))
+    tahun_rilis=input("Masukkan tahun rilis: ")
     harga=input("Masukkan harga: ")
-    stok=int(input("Masukkan stok awal: "))
+    stok=input("Masukkan stok awal: ")
     while ((nama=="") or (kategori=="") or (tahun_rilis=="") or (harga=="") or (stok=="")) :
         print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
         nama=input("Masukkan nama game: ")
         kategori=input("Masukkan kategori: ")
-        tahun_rilis=int(input("Masukkan tahun rilis: "))
+        tahun_rilis=input("Masukkan tahun rilis: ")
         harga=input("Masukkan harga: ")
-        stok=int(input("Masukkan stok awal: "))
+        stok=input("Masukkan stok awal: ")
     #Generator id game
     gid = "GAME"+(idGame.make_id(tahun_rilis,(float(harga)),stok))
     #Menjadikan semua input dan id game sebagai satu baris dengan array
-    lines=["\n", gid, ";", nama,";",kategori,";",str(tahun_rilis),";",str(harga),";",str(stok)]
+    lines=["\n", gid, ";", nama,";",kategori,";",tahun_rilis,";",harga,";",stok]
     print("Selamat! Berhasil menambahkan game", nama)
     f.close()
     return lines
