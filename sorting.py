630651b420ced5d5ee7ec94c4637c8c49ddc5c4b
Najmi Az-Zahra Feryputri <najmi.af24@gmail.com>
2022-04-24 09:54:12 +0700

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -5,14 +5,14 @@ def sortingTahun(skema_sorting):
     #1. MEMBUAT VARIABEL
     f=open("store_game.csv", 'r')
     data = f.readlines()
+    f.close()
     IndEfektif=(banyakBaris("store_game.csv"))-1
     #2. MENGUMPULKAN TAHUN SEBAGAI MATRIKS
-    Msort = ["" for i in range (IndEfektif)]
+    Msort = [["" for j in range (2)] for i in range (IndEfektif)]
     i = 0
     for line in data:
         if parse(line)[3] != "tahun_rilis" :
-            Msort[i] = int(parse(line)[3])
-            #print(Msort[i])
+            Msort[i] = [parse(line)[0],int(parse(line)[3])]
             i = i+1
     #3. SORTING TAHUN DALAM MATRIKS
     if IndEfektif>1 :
@@ -20,61 +20,61 @@ def sortingTahun(skema_sorting):
             IMax = i
             for j in range(i+1,IndEfektif):
                 if skema_sorting == "tahun-" :
-                    if (int(Msort[IMax])<int(Msort[j])):
+                    if (int(Msort[IMax][1])<int(Msort[j][1])):
                         IMax = j
                 elif skema_sorting== "tahun+" :
-                    if (int(Msort[IMax])>int(Msort[j])):
+                    if (int(Msort[IMax][1])>int(Msort[j][1])):
                         IMax = j
-            temp = str(Msort[IMax])
-            Msort[IMax] = str(Msort[i])
-            Msort[i] = str(temp)
+            temp = [str(Msort[IMax][0]), str(Msort[IMax][1])]
+            [Msort[IMax][0], Msort[IMax][1]] = [str(Msort[i][0]), str(Msort[i][1])]
+            [Msort[i][0], Msort[i][1] ] = temp
     #4. MENULISKAN KE LAYAR
     for k in range (IndEfektif) :
         for line in data: 
-            if Msort[k]== parse(line)[3]:
+            if Msort[k][0]==parse(line)[0]:
                 arr = parse(line)
-                print(arr[0],"|",arr[1],"|",arr[2],"|",arr[3],"|",arr[4],"|",arr[5],"|")    
-    f.close()
-
+                print(arr[0],"|",arr[1],"|",arr[2],"|",arr[3],"|",arr[4],"|",arr[5],"|")      
+    
 def sortingHarga(skema_sorting):
     #1. READING DATA, BUAT VARIABEL
     f=open("store_game.csv", 'r')
     data = f.readlines()
+    f.close()
     IndEfektif=(banyakBaris("store_game.csv"))-1
     #2. MENGUMPULKAN HARGA SEBAGAI MATRIKS
     Msort = ["" for i in range (IndEfektif)]
     i = 0
     for line in data:
         if parse(line)[4] != "harga" :
-            Msort[i] = int(parse.parse(line)[4])
+            Msort[i] = [parse(line)[0],int(parse(line)[4])]
             i = i+1
     #3. SORTING HARGA DALAM MATRIKS
     if IndEfektif>1 :
         for i in range(IndEfektif-1):
             IMax = i
             for j in range(i+1,IndEfektif):
-                if skema_sorting=="harga-" :
-                    if (int(Msort[IMax])<int(Msort[j])):
+                if skema_sorting == "harga-" :
+                    if (int(Msort[IMax][1])<int(Msort[j][1])):
                         IMax = j
-                elif skema_sorting=="harga+" :
-                    if (int(Msort[IMax])>int(Msort[j])):
+                elif skema_sorting== "harga+" :
+                    if (int(Msort[IMax][1])>int(Msort[j][1])):
                         IMax = j
-            temp = str(Msort[IMax])
-            Msort[IMax] = str(Msort[i])
-            Msort[i] = str(temp)
+            temp = [str(Msort[IMax][0]), str(Msort[IMax][1])]
+            [Msort[IMax][0], Msort[IMax][1]] = [str(Msort[i][0]), str(Msort[i][1])]
+            [Msort[i][0], Msort[i][1] ] = temp
     #4. MENULISKAN KE LAYAR
     for k in range (IndEfektif) :
         for line in data: 
-            if Msort[k]==(parse(line)[4]):
+            if Msort[k][0]==parse(line)[0]:
                 arr = parse(line)
-                print(arr[0],"|",arr[1],"|",arr[2],"|",arr[3],"|",arr[4],"|",arr[5],"|")    
-    f.close()
+                print(arr[0],"|",arr[1],"|",arr[2],"|",arr[3],"|",arr[4],"|",arr[5],"|")   
 
 def sortingID (skema_sorting):
     if skema_sorting=="" :
         #1. READING DATA, BUAT VARIABEL
         f=open("store_game.csv", 'r')
         data = f.readlines()
+        f.close()
         #2. MENYIMPAN SEMUA ANGKA ID DALAM STRING
         numId = ""
         for line in data:
@@ -106,4 +106,3 @@ def sortingID (skema_sorting):
                 if "GAME"+str(Mid[k])==parse(line)[0]:
                     arr = parse(line)
                     print(arr[0],"|",arr[1],"|",arr[2],"|",arr[3],"|",arr[4],"|",arr[5],"|")    
-        f.close()
