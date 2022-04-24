cebc46a38750ebf72f1f364d88ff675025366b9e
Ilhamnal <104120784+Ilhamnal@users.noreply.github.com>
2022-04-24 19:44:52 +0700

    Update F16.py

--- F16.py
+++ F16.py
@@ -1,38 +1,49 @@
-import time, sys
-from functions import *
 import os
+import csv
+from functions import *
+def save(user, game, riwayat, kepemilikan):
+    jumlah_user = jumlah(user)
+    jumlah_game = jumlah(game)
+    jumlah_riwayat = jumlah(riwayat)
+    jumlah_kepemilikan = jumlah(kepemilikan)
+    savename = input('Masukkan Nama folder: ')
+    try:
+        os.mkdir("./"+savename)
+    except OSError as e:
+        print("Nama Folder sudah ada")
 
-def save(data, data_legend):
-    nama_folder = input('Masukkan nama folder penyimpanan: ')
-    
-    # dapatkan direktori kerja saat ini
-    curr_path = os.getcwd()
-    list_dirs = []
-    isExist = False
-    for (_, sub_dirs, _) in os.walk(curr_path):
-        list_dirs = Array(list_dirs, sub_dirs)
-        break
+    with open('./'+savename+'user.csv','w', newline='') as userfile:
+        head = ['userid','username','nama','password','role','saldo']
+        isiuser = csv.DictWriter(userfile,fieldnames=head,delimiter=',')
+        for i in range (jumlah_user):
+            if i==0 :
+                isiuser.writeheader()
+            else:
+                isiuser.writerow({'userid':user[i][0], 'username':user[i][1], 'nama':user[i][2], 'password':user[i][3], 'role':user[i][4], 'saldo':user[i][5]})
 
-    # loop melalui list_dirs
-    for item in list_dirs:
-        if item == nama_folder:
-            isExist = True
-            break
+    with open('./'+savename+'game.csv','w', newline='') as gamefile:
+        head = ['id','nama','kategori','tahun rilis','harga','stok']
+        isigame = csv.DictWriter(gamefile,fieldnames=head,delimiter=',')
+        for i in range (jumlah_game):
+            if i==0 :
+                isigame.writeheader()
+            else:
+                isigame.writerow({'id':game[i][0], 'nama':game[i][1], 'kategori':game[i][2], 'tahun rilis':game[i][3], 'harga':game[i][4], 'stok':game[i][5]})
 
-    # buat dir baru jika tidak ada
-    if not isExist:
-        os.mkdir(nama_folder)
-    defined_dir_path = curr_path + '\\' + nama_folder
+    with open('./'+savename+'/riwayat.csv','w', newline='') as riwayatfile:
+        head = ['gameid','nama','harga','userid','tahun beli']
+        isiriwayat = csv.DictWriter(riwayatfile,fieldnames=head,delimiter=',')
+        for i in range (jumlah_riwayat):
+            if i==0 :
+                isiriwayat.writeheader()
+            else:
+                isiriwayat.writerow({'gameid':riwayat[i][0], 'nama':riwayat[i][1], 'harga':riwayat[i][2], 'user_id':riwayat[i][3], 'tahun_beli':riwayat[i][4]})
 
-    # tulis data ke folder yang ditentukan
-    for index in range(panjang(data)):
-        temp_file_path = defined_dir_path + '\\' + data_legend[index]
-        overwrite(temp_file_path, data[index])
-
-    print('Loading', end='')
-    sys.stdout.flush()
-    for _ in range(3):
-        time.sleep(2)
-        print('.', end='')
-        sys.stdout.flush()
-    print('\nFile berhasil disimpan.')
+    with open('./'+savename+'kepemilikan.csv','w', newline='') as kepemilikan_file:
+        head = ['gameid','userid']
+        isikepemilikan = csv.DictWriter(kepemilikan_file,fieldnames=head,delimiter=',')
+        for i in range (jumlah_kepemilikan):
+            if i==0 :
+                isikepemilikan.writeheader()
+            else:
+                isikepemilikan.writerow({'gameid':kepemilikan[i][0], 'userid':kepemilikan[i][1]})
