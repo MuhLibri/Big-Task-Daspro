e8622d30ff7796819564c4681a38d3c96f3149c5
Ilhamnal <104120784+Ilhamnal@users.noreply.github.com>
2022-04-24 21:17:01 +0700

    Update F16.py

--- F16.py
+++ F16.py
@@ -1,9 +1,9 @@
 import os
 import csv
 from functions import *
-def save(user, game, riwayat, kepemilikan):
-    jumlah_user = jumlah(user)
-    jumlah_game = jumlah(game)
+def save(username, store_game, riwayat, kepemilikan):
+    jumlah_user = jumlah(username)
+    jumlah_game = jumlah(store_game)
     jumlah_riwayat = jumlah(riwayat)
     jumlah_kepemilikan = jumlah(kepemilikan)
     savename = input('Masukkan Nama folder: ')
@@ -12,38 +12,38 @@ def save(user, game, riwayat, kepemilikan):
     except OSError as e:
         print("Nama Folder sudah ada")
 
-    with open('./'+savename+'user.csv','w', newline='') as userfile:
-        head = ['userid','username','nama','password','role','saldo']
+    with open('./'+savename+'data_login.csv','w', newline='') as userfile:
+        head = ['user_id','nama','username','password','role','saldo']
         isiuser = csv.DictWriter(userfile,fieldnames=head,delimiter=',')
         for i in range (jumlah_user):
             if i==0 :
                 isiuser.writeheader()
             else:
-                isiuser.writerow({'userid':user[i][0], 'username':user[i][1], 'nama':user[i][2], 'password':user[i][3], 'role':user[i][4], 'saldo':user[i][5]})
+                isiuser.writerow({'user_id':username[i][0], 'nama':username[i][1], 'username':username[i][2], 'password':username[i][3], 'role':username[i][4], 'saldo':username[i][5]})
 
-    with open('./'+savename+'game.csv','w', newline='') as gamefile:
-        head = ['id','nama','kategori','tahun rilis','harga','stok']
+    with open('./'+savename+'store_game.csv','w', newline='') as gamefile:
+        head = ['game_id','nama','kategori','tahun_rilis','harga','stok']
         isigame = csv.DictWriter(gamefile,fieldnames=head,delimiter=',')
         for i in range (jumlah_game):
             if i==0 :
                 isigame.writeheader()
             else:
-                isigame.writerow({'id':game[i][0], 'nama':game[i][1], 'kategori':game[i][2], 'tahun rilis':game[i][3], 'harga':game[i][4], 'stok':game[i][5]})
+                isigame.writerow({'game_id':store_game[i][0], 'nama':store_game[i][1], 'kategori':store_game[i][2], 'tahun_rilis':store_game[i][3], 'harga':store_game[i][4], 'stok':store_game[i][5]})
 
-    with open('./'+savename+'/riwayat.csv','w', newline='') as riwayatfile:
-        head = ['gameid','nama','harga','userid','tahun beli']
+    with open('./'+savename+'riwayat.csv','w', newline='') as riwayatfile:
+        head = ['user_id','game_id','nama','harga','tahun_beli']
         isiriwayat = csv.DictWriter(riwayatfile,fieldnames=head,delimiter=',')
         for i in range (jumlah_riwayat):
             if i==0 :
                 isiriwayat.writeheader()
             else:
-                isiriwayat.writerow({'gameid':riwayat[i][0], 'nama':riwayat[i][1], 'harga':riwayat[i][2], 'user_id':riwayat[i][3], 'tahun_beli':riwayat[i][4]})
+                isiriwayat.writerow({'user_id':riwayat[i][0], 'game_id':riwayat[i][1], 'nama':riwayat[i][2], 'harga':riwayat[i][3], 'tahun_beli':riwayat[i][4]})
 
     with open('./'+savename+'kepemilikan.csv','w', newline='') as kepemilikan_file:
-        head = ['gameid','userid']
+        head = ['game_id','user_id']
         isikepemilikan = csv.DictWriter(kepemilikan_file,fieldnames=head,delimiter=',')
         for i in range (jumlah_kepemilikan):
             if i==0 :
                 isikepemilikan.writeheader()
             else:
-                isikepemilikan.writerow({'gameid':kepemilikan[i][0], 'userid':kepemilikan[i][1]})
+                isikepemilikan.writerow({'game_id':kepemilikan[i][0], 'user_id':kepemilikan[i][1]})
